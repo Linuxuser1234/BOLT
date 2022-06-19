7ce303e269748fb10bb551d724424be45a047ab5
SYNC <67390756+awipslinuxuser@users.noreply.github.com>
2022-06-18 20:18:28 -0500

    Delete BOLT directory

--- BOLT/src/main/java/com/example/bolt/BOLT.java
+++
@@ -1,5 +0,0 @@
-package com.example.bolt;
-
-public class BOLT {
-
-}
--- BOLT/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.bolt {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.bolt to javafx.fxml;
-    exports com.example.bolt;
-}

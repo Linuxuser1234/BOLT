fb39366fa1f44d51048f1d02a8c43908e8ac3b3e
Linuxuser1234 <67390756+Linuxuser1234@users.noreply.github.com>
2022-07-22 14:40:08 -0500

    Delete boltgui directory

--- boltgui/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.bolt.boltgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.bolt.boltgui to javafx.fxml;
-    exports com.bolt.boltgui;
-}
